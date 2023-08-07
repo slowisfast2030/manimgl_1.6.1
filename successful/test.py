a8666bc460b54f5353378fe9df9b5e00f9ccd7e7
slowisfast <slowisfast2030@163.com>
2023-08-07 16:40:55 +0800

    slow is fast

--- successful/test.py
+++ successful/test.py
@@ -26,14 +26,16 @@ class test1(Scene):
     def construct(self):
         c = Circle()
         s = Square().center()
+        cc = c.copy().set_color(TEAL)
 
-        c.next_to(s, LEFT, buff=0)
+        c.next_to(s, UP, buff=0)
         self.add(c, s)
         self.wait(1)
 
-        d1 = Dot(np.array([-1., 0., 0.])).set_color(RED)
-        d2 = Dot(np.array([1., 0., 0.])).set_color(BLUE)
-        self.add(d1, d2)
+        d1_target_point = Dot(np.array([0., 1., 0.])).set_color(RED)
+        d2_point_to_align = Dot(np.array([0., -1., 0.])).set_color(BLUE)
+        self.add(d1_target_point, d2_point_to_align)
+        self.add(cc)
 
 
 
