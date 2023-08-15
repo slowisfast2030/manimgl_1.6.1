9158e4e4862144afde6567c576eb335a32a0d22a
Ai Chain <hitthumit@gmail.com>
2023-08-15 18:26:22 +0800

    slow is fast

--- manim_secret/test.py
+++ manim_secret/test.py
@@ -4,6 +4,11 @@ class test_demo(Scene):
     def construct(self):
         c = Circle()
         s = Square()
+        
+        self.add(c,s)
+        print(self.mobjects)
+        
         self.play(Transform(c, s), run_time=1)
         self.wait(1)
+        
                   
