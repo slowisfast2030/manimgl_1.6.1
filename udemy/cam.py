f53ed2ca108bccb6b1dd9b9b8f9615328192716c
Ai Chain <hitthumit@gmail.com>
2023-08-18 09:06:25 +0800

    slow is fast

---
+++ udemy/cam.py
@@ -0,0 +1,29 @@
+from manimlib import *
+import numpy as np
+
+class cam(Scene):
+	def construct(self): 
+		#Scene Material
+		title = Text("Camera").shift(UP*3.5)
+		self.play(FadeIn(title))
+		self.play(FadeIn(Square(fill_opacity=1).set_color(YELLOW)))
+
+		#Camera
+		frame = self.camera.frame
+
+		
+
+		frame4 = frame.copy()
+		frame4.set_width(15)
+		frame4.set_euler_angles(theta=-10*DEGREES, phi=70*DEGREES)
+		self.play(Transform(frame, frame4))
+		#Rotating Camera (without updaters)
+		for i in range(100):
+			frame.increment_theta(0.01)
+			self.wait(0.001)
+
+
+
+
+
+
