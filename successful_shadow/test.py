106c06dbba41396488325148f995228d66c38c2e
Ai Chain <hitthumit@gmail.com>
2023-08-30 12:55:43 +0800

    slow is fast

--- successful_shadow/test.py
+++ successful_shadow/test.py
@@ -53,3 +53,44 @@ class test1(ThreeDScene):
         vg = VGroup(l1, l2, l3)
         #self.add(vg.set_color(TEAL))
         self.wait(5)
+
+class test2(ThreeDScene):
+    def construct(self):
+        frame = self.camera.frame
+        frame.reorient(20, 70)
+        def update_frame(frame, dt):
+            frame.increment_theta(-0.2 * dt)
+
+        frame.add_updater(update_frame)
+
+        axes = ThreeDAxes(x_range=[-3,3], 
+                          y_range=[-3,3],
+                          z_range=[-3,3])
+        self.add(axes)
+
+        def uv_func(u: float, v: float) -> np.ndarray:
+            return np.array([
+                u,
+                v,
+                0
+            ])
+            
+        s = ParametricSurface(uv_func,
+                              u_range=(0,2),
+                              v_range=(-2,0)
+                              )
+        #self.add(s.set_color(BLUE_E).set_opacity(0.5))
+        
+        new_points = []
+        for point in s.get_all_points():
+            x = point[0]
+            y = point[1]
+            if x - y <= 2:
+                new_points.append(point)
+        print(len(s.get_all_points()))
+        print(len(new_points))
+
+        ss = VMobject()
+        ss.set_points(new_points)
+        self.add(ss.set_color(RED))
+        self.wait(5)
