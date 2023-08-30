c6f1b0f4011c74b865ff5860a7841d0fd25163ff
Ai Chain <hitthumit@gmail.com>
2023-08-30 21:58:40 +0800

    slow is fast

--- successful_shadow/test.py
+++ successful_shadow/test.py
@@ -1,26 +1,5 @@
 from manimlib import *
 
-class test(ThreeDScene):
-    def construct(self):
-        frame = self.camera.frame
-        #frame.reorient(20, 70)
-        axes = ThreeDAxes(x_range=[-2,2], y_range=[-3,3])
-        self.add(axes)
-
-        def uv_func(u: float, v: float) -> np.ndarray:
-            return np.array([
-            np.cos(u) * np.sin(v),
-            np.sin(u) * np.sin(v),
-            -np.cos(v)
-            ])
-        
-        s = ParametricSurface(uv_func,
-                              u_range=(0,TAU),
-                              v_range=(0,PI)
-                              )
-        self.add(s.set_color(BLUE_E))
-        self.wait()
-
 class tri_test(ThreeDScene):
     def construct(self):
         frame = self.camera.frame
