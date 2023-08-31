cc36ab248c5ab07fa3d22a1941f7d5d03abcf542
Ai Chain <hitthumit@gmail.com>
2023-08-31 15:45:34 +0800

    slow is fast

--- manimlib/mobject/three_dimensions.py
+++ manimlib/mobject/three_dimensions.py
@@ -47,8 +47,13 @@ class SurfaceMesh(VGroup):
         在start和stop之间均匀采取num个值
         包含start和end
         """
+        # (0, 100, 21)
         u_indices = np.linspace(0, full_nu - 1, part_nu)
+        # (0, 50, 11)
         v_indices = np.linspace(0, full_nv - 1, part_nv)
+        # print(u_indices, v_indices)
+        # [  0.   5.  10.  15.  20.  25.  30.  35.  40.  45.  50.  55.  60.  65. 70.  75.  80.  85.  90.  95. 100.] 
+        # [ 0.  5. 10. 15. 20. 25. 30. 35. 40. 45. 50.]
 
         points, du_points, dv_points = uv_surface.get_surface_points_and_nudged_points()
         normals = uv_surface.get_unit_normals()
