2571619e7571fa0ac0110b897936e2cdb6211048
Ai Chain <hitthumit@gmail.com>
2023-08-31 10:09:03 +0800

    slow is fast

--- successful_shadow/test1.py
+++ successful_shadow/test1.py
@@ -100,27 +100,35 @@ class surface_test(ThreeDScene):
 
         n_lat_lines = 20
         theta_step = PI / n_lat_lines
-        sphere_points = 2.5*np.array([
-            sphere.uv_func(phi, theta + theta_step * (phi / TAU))
-            for theta in np.arange(0, PI, theta_step)
-            for phi in np.linspace(
-                0, TAU, int(2 * n_lat_lines * math.sin(theta)) + 1
-            )
-        ])
         # sphere_points = 2.5*np.array([
-        #     sphere.uv_func(phi, theta)
+        #     sphere.uv_func(phi, theta + theta_step * (phi / TAU))
         #     for theta in np.arange(0, PI, theta_step)
         #     for phi in np.linspace(
         #         0, TAU, int(2 * n_lat_lines * math.sin(theta)) + 1
         #     )
         # ])
+
+        # 固定theta，单层for循环
+        theta = PI/2
+        sphere_points = 2.5*np.array([
+            sphere.uv_func(phi, theta)
+            #for theta in np.arange(0, PI, theta_step)
+            for phi in np.linspace(
+                0, TAU, int(2 * n_lat_lines * math.sin(theta)) + 1
+            )
+        ])
         sphere_points[:, 2] *= -1
         sphere_points += [0,0,1]
+        print(len(sphere_points))
         sphere_dots = DotCloud(sphere_points).set_color(RED)
+        # 为第一个点设置颜色
+        dot_first = Sphere(radius=0.1).move_to(sphere_points[0]).set_color(GREEN)
+
+
         #sphere_dots.set_glow_factor(0.5)
         sphere_dots.make_3d()
 
-        self.add(sphere_dots)
+        self.add(sphere_dots, dot_first)
 
         
 
