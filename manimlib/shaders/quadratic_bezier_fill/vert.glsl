cca8f4c2dfad457d06f3d821839f28d42335c45f
Ai Chain <hitthumit@gmail.com>
2023-09-07 13:48:44 +0800

    10x is easier than 2x

--- manimlib/shaders/quadratic_bezier_fill/vert.glsl
+++ manimlib/shaders/quadratic_bezier_fill/vert.glsl
@@ -2,6 +2,17 @@
 
 #INSERT camera_uniform_declarations.glsl
 
+/*
+这里可以假设我们要绘制的是circle
+在manim中，circle是由8段贝塞尔曲线组成的
+每段贝塞尔曲线由3个控制点组成
+因此，我们有24个控制点
+
+这里我们需要假设:
+我们不仅传入了每个点的坐标，还传入了每个点的法向量(可以提前计算出来)、颜色、索引
+
+在surface的着色器代码中，每个点的法向量是在顶点着色器中计算出来的
+*/
 in vec3 point;
 in vec3 unit_normal;
 in vec4 color;
