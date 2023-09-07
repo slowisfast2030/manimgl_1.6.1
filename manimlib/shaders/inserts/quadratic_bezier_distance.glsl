bfc7861d706ea160312a9a4ffcaafde497085f57
Ai Chain <hitthumit@gmail.com>
2023-09-07 23:23:05 +0800

    10x is easier than 2x

--- manimlib/shaders/inserts/quadratic_bezier_distance.glsl
+++ manimlib/shaders/inserts/quadratic_bezier_distance.glsl
@@ -92,6 +92,8 @@ float dist_to_point_on_curve(vec2 p, float t, vec2 b2){
 这里需要把问题更加准确的定义一下：
 点: p
 贝塞尔曲线: b0(0,0), b1(1,0), b2
+b0, b1, b2三个点会确定一个三角形
+点p在三角形内
 */
 float min_dist_to_curve(vec2 p, vec2 b2, float degree){
     // Check if curve is really a a line
