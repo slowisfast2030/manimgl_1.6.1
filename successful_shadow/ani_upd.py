76ee12bd8a69af461c8bd63c7581365596ed00ae
Ai Chain <hitthumit@gmail.com>
2023-09-08 03:27:48 +0800

    10x is easier than 2x

--- successful_shadow/ani_upd.py
+++ successful_shadow/ani_upd.py
@@ -123,7 +123,7 @@ class glsl(Scene):
 
         贝塞尔曲线内部和外部的判断标准是什么？
         """
-        points = [[0,0,0], [1,0,0], [3.5,1.5,0]] 
+        points = [[0,0,0], [1,0,0], [2,1.5,0]] 
         vm.set_points(np.array(points))
         vm.set_fill(GREEN, 1).set_stroke(WHITE, 0)
         self.add(vm)
