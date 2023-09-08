da4ab1de677bd7333c5937d67a55f1768618147f
Ai Chain <hitthumit@gmail.com>
2023-09-09 04:14:04 +0800

    10x is easier than 2x

--- successful_shadow/bool.py
+++ successful_shadow/bool.py
@@ -2,11 +2,12 @@ from manimlib import *
 
 class bl(Scene):
     def construct(self):
-        c1 = Circle().shift(LEFT*0.7).set_fill(RED, 0.8)
-        c2 = Circle().shift(RIGHT*0.7).set_fill(GREEN, 0.8)
+        c1 = Circle().shift(LEFT*0.7)
+        c2 = Circle().shift(RIGHT*0.7)
         self.add(c1, c2)
 
-        c = Intersection(c1, c2)
+        #c = Intersection(c1, c2).set_fill(TEAL, 0)
+        c = Union(c1, c2).set_fill(TEAL, 0)
         self.add(c)
 
         self.wait()
