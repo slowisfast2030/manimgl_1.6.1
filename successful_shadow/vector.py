70233ff0317fadd6a69fa24bc9f752c5a13d53e1
Ai Chain <hitthumit@gmail.com>
2023-09-11 23:18:41 +0800

    10x is easier than 2x

---
+++ successful_shadow/vector.py
@@ -0,0 +1,34 @@
+from manimlib import *
+
+def pendulum_vector_field_func(theta, omega, mu=0.3, g=9.8, L=3):
+    return [omega, -np.sqrt(g / L) * np.sin(theta) - mu * omega]
+
+class test(Scene):
+	
+	def construct(self): 
+		
+		plane = NumberPlane()
+		
+		vector_field = VectorField(
+			pendulum_vector_field_func,
+			plane,
+			step_multiple=0.5,
+            magnitude_range=(0, 5),
+            length_func=lambda norm: 0.35 * sigmoid(norm)
+		)
+		vector_field.scale(0.5)
+		self.play(FadeIn(vector_field))
+		self.play(FadeOut(vector_field))
+
+		
+		stream_lines = StreamLines(
+			pendulum_vector_field_func,
+			plane
+		)
+		stream_lines.scale(0.5)
+		self.play(FadeIn(stream_lines))
+		self.play(FadeOut(stream_lines))
+		
+		asl = AnimatedStreamLines(stream_lines)
+		self.add(asl)
+		self.wait(3)
