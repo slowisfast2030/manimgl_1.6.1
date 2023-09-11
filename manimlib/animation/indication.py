b7906a7cfa079d3ebca957aa5837d18fad56e411
Ai Chain <hitthumit@gmail.com>
2023-09-11 23:00:07 +0800

    10x is easier than 2x

--- manimlib/animation/indication.py
+++ manimlib/animation/indication.py
@@ -87,6 +87,15 @@ class Indicate(Transform):
         return target
 
 
+"""
+class ShowFlash(Scene):
+    def construct(self):
+        dot = Dot(ORIGIN, color=YELLOW)
+        dot.set_stroke(width=0)
+        dot.set_fill(opacity=0)
+        self.play(Flash(dot, flash_radius=0.8, line_length=0.6, run_time=2))
+        self.wait()
+"""
 class Flash(AnimationGroup):
     CONFIG = {
         "line_length": 0.2,
