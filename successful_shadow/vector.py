7874d84d9486c92c05951e5099b2d3aafcb60172
Ai Chain <hitthumit@gmail.com>
2023-09-12 11:28:28 +0800

    10x is easier than 2x

--- successful_shadow/vector.py
+++ successful_shadow/vector.py
@@ -18,18 +18,19 @@ class test(Scene):
             length_func=lambda norm: 0.35 * sigmoid(norm)
 		)
 		vector_field.scale(0.5)
-		self.play(FadeIn(vector_field))
-		self.play(FadeOut(vector_field))
+		self.add(vector_field)
+		# self.play(FadeIn(vector_field))
+		# self.play(FadeOut(vector_field))
 
 		
-		stream_lines = StreamLines(
-			pendulum_vector_field_func,
-			plane
-		)
-		stream_lines.scale(0.5)
-		self.play(FadeIn(stream_lines))
-		self.play(FadeOut(stream_lines))
+		# stream_lines = StreamLines(
+		# 	pendulum_vector_field_func,
+		# 	plane
+		# )
+		# stream_lines.scale(0.5)
+		# self.play(FadeIn(stream_lines))
+		# self.play(FadeOut(stream_lines))
 		
-		asl = AnimatedStreamLines(stream_lines)
-		self.add(asl)
-		self.wait(3)
+		# asl = AnimatedStreamLines(stream_lines)
+		# self.add(asl)
+		# self.wait(3)
