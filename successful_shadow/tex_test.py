099f490e6242b092f96e4d409f8755eea95a81b6
Ai Chain <hitthumit@gmail.com>
2023-09-14 15:04:32 +0800

    10x is easier than 2x

--- successful_shadow/tex_test.py
+++ successful_shadow/tex_test.py
@@ -2,7 +2,7 @@ from manimlib import *
 
 class test(Scene):
     def construct(self):
-        to_isolate = ["B", "C", "=", "(", ")"]
+        to_isolate = ["B^2", "C^2", "=", "+", "A^2"]
 
         # t = Tex("A^2", "+", "B^2", "=", "C^2")
         # self.add(t)
