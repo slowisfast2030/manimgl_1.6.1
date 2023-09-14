4c98e3cc1a88c51b6ec3d4bcd0a3cb09e56c1d99
Ai Chain <hitthumit@gmail.com>
2023-09-14 16:08:05 +0800

    10x is easier than 2x

--- successful_shadow/tex_test.py
+++ successful_shadow/tex_test.py
@@ -8,6 +8,7 @@ class test(Scene):
         to_isolate = ["+"]
 
         # Tex类传入的文本中间的空格会被处理, 多个空格会被合并为一个空格
+        # 传入isolate后，文本会被isolate中给定的字符进行分隔
         c = Tex("A^2 + B^2 = C^2", isolate=to_isolate)
     
         for index, smob in  enumerate(c.submobjects):
