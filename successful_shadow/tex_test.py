b06b9aa8018cd640445b41c6cea6903d1301eeb9
Ai Chain <hitthumit@gmail.com>
2023-09-14 16:07:04 +0800

    10x is easier than 2x

--- successful_shadow/tex_test.py
+++ successful_shadow/tex_test.py
@@ -4,11 +4,14 @@ class test(Scene):
     def construct(self):
         #to_isolate = ["A^2", "B^2", "C^2", "=", "+"]
         #to_isolate = ["A^2", "B^2", "C^2"]
-        to_isolate = ["A^2", "C^2"]
+        #to_isolate = ["A^2", "C^2"]
+        to_isolate = ["+"]
 
-        c = Tex("A^2      + B^2 = C^2", isolate=to_isolate)
+        # Tex类传入的文本中间的空格会被处理, 多个空格会被合并为一个空格
+        c = Tex("A^2 + B^2 = C^2", isolate=to_isolate)
     
         for index, smob in  enumerate(c.submobjects):
             print(f"Index {index}: Value {smob}      String: {smob.tex_string}")
             self.add(smob.shift(0.5*index*DOWN))
+            #self.add(smob)
         
