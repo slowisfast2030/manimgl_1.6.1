124d784c4ea73016fe8a52cfd31af91d49e17b22
Ai Chain <hitthumit@gmail.com>
2023-09-14 15:44:44 +0800

    10x is easier than 2x

--- successful_shadow/tex_test.py
+++ successful_shadow/tex_test.py
@@ -9,6 +9,6 @@ class test(Scene):
         c = Tex("A^2 + B^2 = C^2", isolate=to_isolate)
     
         for index, smob in  enumerate(c.submobjects):
-            print(index, smob, smob.tex_string)
+            print(f"Index {index}: Value {smob}      String: {smob.tex_string}")
             self.add(smob.shift(0.5*index*DOWN))
         
