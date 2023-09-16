0efdcd5204fa6257252580f5bb2b03bd1c6c2a1b
Ai Chain <hitthumit@gmail.com>
2023-09-16 12:59:19 +0800

    10x is easier than 2x

--- successful_pi/pi_test.py
+++ successful_pi/pi_test.py
@@ -3,10 +3,19 @@ sys.path.append('/Users/linus/Desktop/slow-is-fast/manimgl_1.6.1/3b1b-videos-mas
 
 from manim_imports_ext import *
 
+LEFT_EYE_INDEX: int = 0
+RIGHT_EYE_INDEX: int = 1
+LEFT_PUPIL_INDEX: int = 2
+RIGHT_PUPIL_INDEX: int = 3
+BODY_INDEX: int = 4
+MOUTH_INDEX: int = 5
+
 class test(Scene):
     def construct(self):
         pi = PiCreature(color=BLUE_E)
-        print(pi.parts)
-        self.add(pi.parts[0], pi.parts[2])
+        parts = pi.parts
 
-        self.add(pi.submobjects[0].copy().shift(DOWN))
+        original_irises=VGroup(parts[LEFT_EYE_INDEX], parts[RIGHT_EYE_INDEX])
+        original_pupils=VGroup(parts[LEFT_PUPIL_INDEX], parts[RIGHT_PUPIL_INDEX])
+        self.add(original_irises)
+        #self.add(original_pupils)
