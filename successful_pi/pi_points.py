deacdce8fccdf2ec53ce8f5cf62a34e1e865f8f4
Ai Chain <hitthumit@gmail.com>
2023-09-19 21:43:02 +0800

    10x is easier than 2x

--- successful_pi/pi_points.py
+++ successful_pi/pi_points.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append('/Users/linus/Desktop/slow-is-fast/manimgl_1.6.1/3b1b-videos-master')
 
 from manim_imports_ext_new import *
+import yaml
+
 
 class test(Scene):
     def construct(self):
@@ -10,6 +12,26 @@ class test(Scene):
 
         modes = ('plain', 'happy', 'sad')
         pi = PiCreature(mode='plain')
-        mob = pi[0][0][0]
-        self.add(mob)
-        print(mob.get_all_points())
+        mob = pi
+        
+        eyes = mob.eyes
+        left_iris = eyes[0][0]
+        left_pupil = eyes[0][1]
+        right_iris = eyes[1][0]
+        right_pupil = eyes[1][1]
+        mouth = mob.mouth
+
+        self.add(mob, )
+        data_to_write = {
+            "plain": {
+                "iris_left": left_iris.get_points(),
+                "pupil_left": left_pupil.get_all_points(),
+                "iris_right": right_iris.get_points(),
+                "pupil_right": right_pupil.get_all_points(),
+                "mouth": mouth.get_points()
+                }
+        }
+
+        with open("alphabet_points.yml", "w") as yaml_file:
+            yaml.dump(data_to_write, yaml_file)
+        
