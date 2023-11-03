6769cc6094602cc9ed61b494f67b60ed86cf5249
Ai Chain <hitthumit@gmail.com>
2023-11-03 13:52:24 +0800

    slow is fast

--- successful/deep_thinking.py
+++ successful/deep_thinking.py
@@ -3,7 +3,9 @@ from manimlib import *
 class test(Scene):
     def construct(self):
         c = Circle().shift(LEFT)
-        partial_circle = Arc(start_angle=0, angle=PI)
         s = Square().shift(RIGHT)
+
+        partial_circle = Arc(start_angle=0, angle=PI)
         line = Line(RIGHT*2, LEFT*2)
+        
         self.play(Transform(partial_circle, line))
