3c8537f42f02a68339ba95a322cf86845916970c
Ai Chain <hitthumit@gmail.com>
2023-11-03 13:56:02 +0800

    slow is fast

--- successful/deep_thinking.py
+++ successful/deep_thinking.py
@@ -6,6 +6,8 @@ class test(Scene):
         s = Square().shift(RIGHT)
 
         partial_circle = Arc(start_angle=0, angle=PI)
-        line = Line(RIGHT*2, LEFT*2).shift(LEFT*3)
+        # 需要这里起始点的顺序，否则会出现不合理的插值结果
+        line1 = Line(RIGHT*2, LEFT*2).shift(LEFT*3)
+        line2 = Line(LEFT*2, RIGHT*2).shift(LEFT*3)
 
-        self.play(Transform(partial_circle, line))
+        self.play(Transform(partial_circle, line2))
