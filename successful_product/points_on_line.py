560e10c0c55a27948c656cc257177210a9ee9a15
Ai Chain <hitthumit@gmail.com>
2023-11-08 10:43:02 +0800

    把事情作对的方法论：做一个完整作品

--- successful_product/points_on_line.py
+++ successful_product/points_on_line.py
@@ -33,3 +33,38 @@ class PointsOnLine(Scene):
             Dot(line.point_from_proportion(i / max(num_points, 1)), radius=0.05)
             for i in range(1, num_points)
         ])
+
+# from gpt4
+class test(Scene):
+    def construct(self):
+        # Define the start and end points of the line
+        line_start = LEFT * 4
+        line_end = RIGHT * 4
+        line = Line(line_start, line_end)
+        self.add(line)
+
+        # Initial density and time
+        density = 1
+        time = 0
+        increment = 0.1  # Increment the density linearly with time
+        points = VGroup()
+
+        # Add points on the line
+        def update_points(dt):
+            nonlocal time, density
+            time += dt
+            density += increment * dt
+            new_points = self.get_dots_on_line(density, line)
+            points.become(new_points)
+
+        points.add_updater(update_points)
+        self.add(points)
+
+        self.wait(5)  # The animation will last for 5 seconds
+
+    def get_dots_on_line(self, density, line):
+        num_points = int(density * line.get_length())
+        return VGroup(*[
+            Dot(line.point_from_proportion(i / max(num_points, 1)), radius=0.05)
+            for i in range(1, num_points)
+        ])
