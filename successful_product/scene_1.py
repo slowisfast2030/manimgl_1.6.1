55924c5ba04100b04fba4ff183c3e4200e77c34b
Ai Chain <hitthumit@gmail.com>
2023-11-09 10:15:54 +0800

    完整作品

--- successful_product/scene_1.py
+++ successful_product/scene_1.py
@@ -8,6 +8,18 @@ from manimlib import *
 整个视角一开始是俯视图
 然后以特定角度观察
 """
-class test(Scene):
+class test(ThreeDScene):
     def construct(self) -> None:
-        pass
+        frame = self.camera.frame
+        # frame.reorient(20, 70)
+        # def update_frame(frame, dt):
+        #     frame.increment_theta(-0.2 * dt)
+        # frame.add_updater(update_frame)
+        
+        axes = ThreeDAxes(x_range=[-5, 5, 1], 
+                        y_range=[-5, 5, 1], 
+                        z_range=[-5, 5, 1])
+        self.add(axes)
+
+        rec = Rectangle(width=4, height=4).set_fill(BLUE_E)
+        self.add(rec)
