343ba6919c08d692ab57ef6a56bf1b62295126f6
Ai Chain <hitthumit@gmail.com>
2023-11-10 21:38:45 +0800

    完整作品

--- successful_product/frame.py
+++ successful_product/frame.py
@@ -8,4 +8,26 @@ class test(ThreeDScene):
             frame.increment_theta(-0.2 * dt)
         frame.add_updater(update_frame)
 
+        axes = ThreeDAxes(x_range=[-5, 5, 1],
+                          y_range=[-5, 5, 1],
+                          z_range=[-3.5, 4, 1],
+                          axis_config={"include_tip": True, "tick_size": 0.05})
+        self.add(axes)
+
+        def uv_func(u:float, v:float) -> np.ndarray:
+            return np.array([
+                u,
+                v,
+                u**2 + v**2 - 2
+            ])
+
+        s = ParametricSurface(
+            uv_func,
+            u_range=[-2, 2],
+            v_range=[-2, 2]
+        ).set_color(BLUE_E).set_opacity(0.5)
+        
+        self.add(s)
+
+        self.wait(3)
         
