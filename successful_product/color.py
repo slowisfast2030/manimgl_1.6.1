48319504c17ba92b340b5cb7708ea423892e1971
Ai Chain <hitthumit@gmail.com>
2023-11-10 15:38:32 +0800

    完整作品

---
+++ successful_product/color.py
@@ -0,0 +1,21 @@
+from manimlib import *
+
+class test(Scene):
+    def construct(self):
+        # Create the integral expression
+        integral_tex = Tex(r"\int_a^b f(x) \,dx = S", 
+                               isolate=["\int", "a", "b", "f(x)", "dx", "S"])
+
+        # Apply different colors to each part of the expression
+        integral_tex.set_color_by_tex_to_color_map({
+            "\int": RED,
+            "a": GREEN,
+            "b": BLUE,
+            "f(x)": YELLOW,
+            "dx": ORANGE,
+            "S": PURPLE
+        })
+
+        # Display the expression
+        self.play(Write(integral_tex))
+        self.wait()
