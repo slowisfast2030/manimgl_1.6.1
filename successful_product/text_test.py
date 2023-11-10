d45eab846dcf044277102412f7542cafeada5ef7
Ai Chain <hitthumit@gmail.com>
2023-11-10 14:38:46 +0800

    完整作品

--- successful_product/text_test.py
+++ successful_product/text_test.py
@@ -6,9 +6,11 @@ class test(Scene):
     
         tex = Tex("E = MC^2").shift(DOWN)
 
-        textext = TexText("""
-            Or thinking of the plane as $\\mathds{C}$,\\\\
-            this is the map $z \\rightarrow z^2$
-        """).shift(DOWN*2.5)
+        # textext = TexText("""
+        #     Or thinking of the plane as $\\mathds{C}$,\\\\
+        #     this is the map $z \\rightarrow z^2$
+        # """).shift(DOWN*2.5)
+        textext = TexText("This is a combination of text and a LaTeX formula: $E=mc^2$").shift(DOWN*2)
+
 
         self.add(text, tex, textext)
