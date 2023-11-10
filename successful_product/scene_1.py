616d2874725e03d50cd7822606ea5dd24a7b0270
Ai Chain <hitthumit@gmail.com>
2023-11-10 16:06:19 +0800

    完整作品

--- successful_product/scene_1.py
+++ successful_product/scene_1.py
@@ -53,8 +53,10 @@ class test(ThreeDScene):
         
         eq = Tex("x^2", "+", "y^2", "=", "9").shift(DOWN*3.5+LEFT*3).scale(1) 
         integral = Tex(r"\int_{\text{circle}} f(x,y) \,ds").shift(DOWN*3.5+RIGHT*3).scale(1)
+        eq_arrow = Arrow(LEFT, RIGHT).scale(0.5).next_to(eq, LEFT)
         self.play(ShowCreation(curve_down),
                   FadeIn(eq), 
+                  FadeIn(eq_arrow),
                   FadeIn(integral),
                   run_time=1)
         
