087ce615816f17ea0aae7d2b6b4dbf4dd5ae0eef
Ai Chain <hitthumit@gmail.com>
2023-11-10 14:26:27 +0800

    完整作品

---
+++ successful_product/text_test.py
@@ -0,0 +1,11 @@
+from manimlib import *
+
+class test(Scene):
+    def construct(self):
+        text = Text("E = MC^2")
+        self.add(text)
+        textext = TexText("""
+            Or thinking of the plane as $\\mathds{C}$,\\\\
+            this is the map $z \\rightarrow z^2$
+        """).shift(DOWN)
+        self.add(textext)
