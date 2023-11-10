7f11e64e1bb6dcb96b499695add9e4842c587b84
Ai Chain <hitthumit@gmail.com>
2023-11-10 11:49:34 +0800

    完整作品

---
+++ successful_product/3d.py
@@ -0,0 +1,12 @@
+from manimlib import *
+
+class test(Scene):
+    def construct(self):
+        frame = self.camera.frame
+        frame.reorient(20, 70)
+        def update_frame(frame, dt):
+            frame.increment_theta(-0.2 * dt)
+        frame.add_updater(update_frame)
+        line = Line3D(LEFT, RIGHT, color=RED)
+        self.add(line)
+        self.wait(3)
