cff04cffac8af2281f87c0f5b6dcca3aa16a6e17
Ai Chain <hitthumit@gmail.com>
2023-11-10 14:28:45 +0800

    完整作品

--- successful_product/text_test.py
+++ successful_product/text_test.py
@@ -4,8 +4,9 @@ class test(Scene):
     def construct(self):
         text = Text("E = MC^2")
         self.add(text)
-        textext = TexText("""
-            Or thinking of the plane as $\\mathds{C}$,\\\\
-            this is the map $z \\rightarrow z^2$
-        """).shift(DOWN)
+        # textext = TexText("""
+        #     Or thinking of the plane as $\\mathds{C}$,\\\\
+        #     this is the map $z \\rightarrow z^2$
+        # """).shift(DOWN)
+        textext = Tex("E = MC^2").shift(DOWN)
         self.add(textext)
