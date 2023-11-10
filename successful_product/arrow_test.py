69e867d88f6338aecbbb39397a9b9fc47239b27e
Ai Chain <hitthumit@gmail.com>
2023-11-10 15:15:23 +0800

    完整作品

--- successful_product/arrow_test.py
+++ successful_product/arrow_test.py
@@ -4,3 +4,9 @@ class test(Scene):
     def construct(self):
         arrow = Arrow(LEFT, RIGHT)
         self.add(arrow)
+
+        elbow = Elbow().shift(UP)
+        self.add(elbow)
+
+        fillarrow = FillArrow(LEFT, RIGHT, color=RED).shift(DOWN)
+        self.add(fillarrow)
