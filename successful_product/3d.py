49a871e6f3cbf5f53a4d74be759e01a32246f796
Ai Chain <hitthumit@gmail.com>
2023-11-10 11:49:53 +0800

    完整作品

--- successful_product/3d.py
+++ successful_product/3d.py
@@ -7,6 +7,7 @@ class test(Scene):
         def update_frame(frame, dt):
             frame.increment_theta(-0.2 * dt)
         frame.add_updater(update_frame)
+        
         line = Line3D(LEFT, RIGHT, color=RED)
         self.add(line)
         self.wait(3)
