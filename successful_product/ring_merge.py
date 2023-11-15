7c2b298ab96a55e81d4a619bde8109225a87ace0
Ai Chain <hitthumit@gmail.com>
2023-11-15 13:26:20 +0800

    完整作品

--- successful_product/ring_merge.py
+++ successful_product/ring_merge.py
@@ -2,15 +2,18 @@ from manimlib import *
 
 class RingMerge(Scene):
     def construct(self):
-        ring = self.get_ring(1, 0.2).move_to(LEFT)
+        ring = self.get_ring(1, 0.2, color = BLUE)
         self.add(ring)
         print(ring.get_all_points())
+        c = Circle(radius = 1.2).set_stroke(width = 0.5).set_color(RED)
+        c.shift(LEFT*3)
+        self.add(c)
     
-    def get_ring(self, radius, dR, color = GREEN):
+    def get_ring(self, radius, dR, color = RED):
         ring = Circle(radius = radius + dR).center()
         inner_ring = Circle(radius = radius)
         inner_ring.rotate(np.pi, RIGHT)
         ring.append_vectorized_mobject(inner_ring)
-        ring.set_stroke(width = 0)
+        ring.set_stroke(width = 0.5)
         ring.set_fill(color)
         return ring
