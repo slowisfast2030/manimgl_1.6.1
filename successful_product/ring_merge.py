e0007e808ce2d67a0d776d78c3a3fe4b6e9f7ad2
Ai Chain <hitthumit@gmail.com>
2023-11-15 13:38:55 +0800

    完整作品

--- successful_product/ring_merge.py
+++ successful_product/ring_merge.py
@@ -4,17 +4,12 @@ class test(Scene):
     def construct(self):
         ring = self.get_ring(1, 0.2)
         self.add(ring)
-        print(ring.get_all_points())
-        
-        c = Circle(radius = 1.2).set_stroke(width = 0.5).set_fill(RED)
-        c.shift(LEFT*3)
-        self.add(c)
     
     def get_ring(self, radius, dR, color = RED):
         ring = Circle(radius = radius + dR).center()
         inner_ring = Circle(radius = radius).center()
-        inner_ring.rotate(np.pi, RIGHT)
+        inner_ring.rotate(PI, RIGHT)
         ring.append_vectorized_mobject(inner_ring)
         ring.set_stroke(width = 0.5)
-        ring.set_fill(color)
+        ring.set_fill(color,1)
         return ring
