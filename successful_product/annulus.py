5bba07a64d459ed0bfc255db6532466b0d7527ea
Ai Chain <hitthumit@gmail.com>
2023-11-15 13:35:06 +0800

    完整作品

--- successful_product/annulus.py
+++ successful_product/annulus.py
@@ -4,3 +4,6 @@ class test(Scene):
     def construct(self):
         rings = AnnularSector(inner_radius=1, outer_radius=2, angle=4/4*PI).set_fill(BLUE)
         self.add(rings)
+        c = Square().set_fill(RED, 1)
+        c.shift(LEFT*3)
+        self.add(c)
