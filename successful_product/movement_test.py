6edd1f281a063f82aea4ae87ac63c73e9a01b23b
Ai Chain <hitthumit@gmail.com>
2023-11-18 23:22:23 +0800

    完整作品

--- successful_product/movement_test.py
+++ successful_product/movement_test.py
@@ -20,3 +20,35 @@ class test(Scene):
 
         # Play the animation
         self.play(translation_animation)
+
+
+class RotationAnimation(Scene):
+    def construct(self):
+        # Create a Mobject, e.g., a square
+        square = Square()
+
+        # Define the rotation function
+        # This function rotates the object around the origin
+        def rotation_function(p):
+            # Define the rotation angle per unit time (in radians)
+            angle = 1  # Adjust this for different rotation speeds
+            rotation_matrix = np.array([
+                [np.cos(angle), -np.sin(angle), 0],
+                [np.sin(angle), np.cos(angle), 0],
+                [0, 0, 1]
+            ])
+            return np.dot(rotation_matrix, p) - p
+
+        # Create a PhaseFlow animation
+        rotation_animation = PhaseFlow(
+            function=rotation_function,
+            mobject=square,
+            virtual_time=1,  # Adjust as needed
+            rate_func=linear
+        )
+
+        # Play the animation
+        self.play(rotation_animation)
+
+        # Keep the scene displayed after the animation
+        self.wait()
