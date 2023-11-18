36bf4a045adb1d85e0b8c513c2339b68ad419b41
Ai Chain <hitthumit@gmail.com>
2023-11-19 00:10:52 +0800

    完整作品

---
+++ successful_product/position.py
@@ -0,0 +1,15 @@
+from manimlib import *  # or from manimlib import * for ManimGL
+
+
+class ExampleScene(Scene):
+    def construct(self):
+        # Create two mobjects
+        circle = Circle(color=BLUE)
+        square = Square(color=RED).shift(RIGHT*1.9)
+        self.add(circle, square)
+
+        circle.add_updater(lambda m: m.shift(0.1* LEFT))
+
+        # Animate the square to maintain its position relative to the circle
+        self.play(MaintainPositionRelativeTo(square, circle), run_time=3)
+
