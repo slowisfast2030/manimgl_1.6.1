54bc40e85537ac1a71dafc4b8437ee45676a66ec
Ai Chain <hitthumit@gmail.com>
2023-11-19 14:52:50 +0800

    完整作品

--- successful_product/frame_test.py
+++ successful_product/frame_test.py
@@ -5,7 +5,8 @@ class FrameTest(Scene):
         c = Circle()
         s = Square()
         frame = self.camera.frame
-        frame.set_height(8)
+        #frame.set_height(8)
+        frame.move_to(np.array([0,0,5]))
 
         frame_copy = frame.copy()
         frame_copy.scale(0.5).shift(UP+RIGHT)
