41dd1134c8d0e36657e83ae407abd5c9411b5f65
Ai Chain <hitthumit@gmail.com>
2023-11-19 15:08:55 +0800

    完整作品

--- successful_product/frame_test.py
+++ successful_product/frame_test.py
@@ -2,10 +2,9 @@ from manimlib import *
 
 class FrameTest(Scene):
     def construct(self):
-        #c = Circle().move_to([0,0,10])
         c = Circle()
         s = Square()
-        self.add(c)
+        
         frame = self.camera.frame
         frame.set_height(8)
         frame.move_to(np.array([0,0,5]))
@@ -14,5 +13,4 @@ class FrameTest(Scene):
         frame_copy.scale(0.5).shift(UP+RIGHT)
         self.add(frame_copy)
 
-        #self.play(Transform(c, s))
-        self.play(c.animate.shift(LEFT))
+        self.play(Transform(c, s))
