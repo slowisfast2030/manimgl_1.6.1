63b64390d63c6d622d71c06a128b92b7618c6b58
Ai Chain <hitthumit@gmail.com>
2023-11-20 12:34:39 +0800

    完整作品

--- successful_tiktok/word.py
+++ successful_tiktok/word.py
@@ -6,13 +6,15 @@ from manim_imports_ext import *
 class test(Scene):
     def construct(self):
 
-        pi2 = PiCreature(color=BLUE_E)
-        self.add(pi2)
-        pi2.move_to(DOWN*2)
+        colors = color_gradient([BLUE, GREEN], 3)
+        pis = [PiCreature(color=color).scale(0.5) for color in colors]
+        pi_group = VGroup(*pis)
+        pi_group.arrange(RIGHT).shift(DOWN)
+        self.add(pi_group)
         self.wait()
 
-        pi2.look_at(LEFT)
-        self.wait()
+        # pi2.look_at(LEFT)
+        # self.wait()
 
-        pi2.shrug()
-        self.wait()
+        # pi2.shrug()
+        # self.wait()
