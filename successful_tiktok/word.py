94a141d9ca5c8036aba7bfc6ad5fc01b6077ff4f
Ai Chain <hitthumit@gmail.com>
2023-11-20 12:47:15 +0800

    完整作品

--- successful_tiktok/word.py
+++ successful_tiktok/word.py
@@ -7,14 +7,20 @@ class test(Scene):
     def construct(self):
 
         colors = color_gradient([BLUE, GREEN], 3)
-        pis = [PiCreature(color=color).scale(0.5) for color in colors]
+        pis = [PiCreature(color=color).scale(0.8) for color in colors]
         pi_group = VGroup(*pis)
-        pi_group.arrange(RIGHT).shift(DOWN)
+        pi_group.arrange(RIGHT).shift(DOWN*5)
         self.add(pi_group)
         self.wait()
+        word = Text("Abandon").scale(2).shift(UP*6.5+LEFT*2.2).set_color(RED)
+        self.play(Write(word))
 
-        # pi2.look_at(LEFT)
-        # self.wait()
+        pis[0].look_at(LEFT)
+        pis[1].shrug()
+        self.wait()
 
-        # pi2.shrug()
-        # self.wait()
+        meaning_1 = Text("V-T If you abandon a place, thing, or person, you leave the place, thing, or person permanently or for a long time, especially when you should not do so.         •  He claimed that his parents had abandoned him.")
+        meaning_1.next_to(word, DOWN)
+        self.add(meaning_1)
+        self.wait()
+        
