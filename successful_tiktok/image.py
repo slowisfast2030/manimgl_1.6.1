6e79f858ecb50794d0fa46cf22eaa4d2aa43298f
Ai Chain <hitthumit@gmail.com>
2023-11-21 12:12:35 +0800

    完整作品

--- successful_tiktok/image.py
+++ successful_tiktok/image.py
@@ -2,8 +2,11 @@ from manimlib import *
 
 class test(Scene):
     def construct(self):
-        image = ImageMobject("dall-path.png").scale(3)
-        self.add(image)
+        image_path = ImageMobject("dall-path.png").scale(2).shift(IN*4)
+        image_house = ImageMobject("dall-house.png").scale(2)
+        image_boy = ImageMobject("dall-boy.png").scale(2).shift(OUT*4)
+
+        self.add(image_path, image_house, image_boy)
 
         frame = self.camera.frame
         def update_frame(frame, dt):
@@ -11,5 +14,5 @@ class test(Scene):
 
         self.play(frame.animate.reorient(30, 70), run_time=2)
         frame.add_updater(update_frame)
-        self.wait(3)
+        self.wait(5)
 
