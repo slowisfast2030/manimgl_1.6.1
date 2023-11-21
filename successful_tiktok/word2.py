ad70917c2886ab02d79aeec0983983a315df01f1
Ai Chain <hitthumit@gmail.com>
2023-11-21 13:41:09 +0800

    完整作品

--- successful_tiktok/word2.py
+++ successful_tiktok/word2.py
@@ -62,5 +62,34 @@ class test(Scene):
 
 
 
-        
+        image_path = ImageMobject("dall-path.png").scale(1.5)
+        image_house = ImageMobject("dall-house.png").scale(1.5)
+        image_boy = ImageMobject("dall-boy.png").rotate(PI/2).scale(1.5)
+
+        radius = image_path.get_height()/2
+        image_path.move_to(radius * OUT)
+        image_house.move_to(radius * OUT)
+        image_boy.move_to(radius * OUT)
+
+        result = [image_path]
+        # result.extend([
+        #     image_path.copy().rotate(PI / 2, axis=vect, about_point=ORIGIN)
+        #     for vect in compass_directions(4)
+        # ])
+        result.append(image_house.copy().rotate(PI/2, RIGHT, about_point=ORIGIN))
+        result.append(image_boy.copy().rotate(PI/2, UP, about_point=ORIGIN))
+
+        self.add(*result)   
+
+        word_fix = Text("Abandon", t2w={"Abandon": BOLD}).scale(3).set_color(BLUE_B).shift(UP*3)
+        word_fix.fix_in_frame()
+
+        frame = self.camera.frame
+        def update_frame(frame, dt):
+            frame.increment_theta(-0.1 * dt)
+
+        self.play(frame.animate.reorient(60, 70),Write(word_fix), run_time=2)
+        frame.add_updater(update_frame)
+
+        self.wait(5)
         
