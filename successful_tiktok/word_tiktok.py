97b65c2ec9e8120aecfc3d749d95f790d65abd26
Ai Chain <hitthumit@gmail.com>
2023-11-24 23:04:07 +0800

    完整作品

--- successful_tiktok/word_tiktok.py
+++ successful_tiktok/word_tiktok.py
@@ -230,4 +230,5 @@ class test(Scene):
         self.play(
             *[Write(sent) for sent in sentence_gr])
 
-
+        image_boy = image_divide("dall-boy.png", 10, 10).shift(DOWN*3).space_out_submobjects(1.01)
+        self.add(*image_boy)
