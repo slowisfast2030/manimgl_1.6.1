3da61c02f5e3d160eec665c62b3b6153d658775f
Ai Chain <hitthumit@gmail.com>
2023-11-24 14:54:56 +0800

    完整作品

--- successful_tiktok/image_surface.py
+++ successful_tiktok/image_surface.py
@@ -21,7 +21,7 @@ class test(Scene):
         pis = [PiCreature(color=color) for color in colors]
         pis[1].set_color(RED).scale(1.3).shift(UP*1.2)
         pi_group = VGroup(*pis)
-        pi_group.arrange(RIGHT, buff=0.9).shift(DOWN*6).scale(0.8)
+        pi_group.arrange(RIGHT, buff=0.9).shift(DOWN*5.5).scale(0.8)
         #self.play(FadeIn(pi_group))
         self.add(pi_group)
         self.play(pis[1].says("today, we will \nleran abandon!"))
@@ -37,18 +37,14 @@ class test(Scene):
         # in whatever you've set as the image directory in
         # the custom_config.yml file
 
-        # day_texture = "EarthTextureMap"
-        # night_texture = "NightEarthTextureMap"
+    
         texture1 = "dall-boy.png"
-        #texture = "../successful_product/EarthTextureMap.jpeg"
         mob1 = TexturedSurface(sphere1, texture1).scale(0.3).rotate(PI/2, axis=RIGHT)
 
         texture2 = "dall-house.png"
-        #texture = "../successful_product/EarthTextureMap.jpeg"
         mob2 = TexturedSurface(sphere2, texture2).scale(0.3).rotate(PI/2, axis=RIGHT)
 
         texture3 = "dall-path.png"
-        #texture = "../successful_product/EarthTextureMap.jpeg"
         mob3 = TexturedSurface(sphere3, texture3).scale(0.3).rotate(PI/2, axis=RIGHT)
 
         gr = Group(mob1, mob2, mob3).arrange(RIGHT, buff=0.5).scale(0.4).move_to([2.7,6.7,0])
