f2f32b79cdef352733f6036301fd00e41e6e4aaa
Ai Chain <hitthumit@gmail.com>
2023-11-28 01:06:04 +0800

    完整作品

--- 3b1b-videos-master/_2017/eoc/chapter1.py
+++ 3b1b-videos-master/_2017/eoc/chapter1.py
@@ -307,6 +307,7 @@ class Introduction(TeacherStudentsScene):
         # 可以为每一部分设置颜色
         words.set_color_by_tex("Essence of calculus", YELLOW)
 
+        # change_mode不是一个动画
         self.teacher.change_mode("happy")
         self.play(
             FadeIn(
