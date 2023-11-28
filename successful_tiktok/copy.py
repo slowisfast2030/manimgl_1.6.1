8dafc8351bf894244a3f956d72aa8a6747f24281
Ai Chain <hitthumit@gmail.com>
2023-11-28 13:32:24 +0800

    完整作品

--- successful_tiktok/copy.py
+++ successful_tiktok/copy.py
@@ -2,26 +2,25 @@ from manimlib import *
 
 class test(Scene):
     def construct(self):
-        # Create a square
-        square = Square()
+        # Create a VGroup with a square and a circle
+        group = VGroup(Square(), Circle()).arrange(RIGHT)
 
-        # Shallow copy of the square
-        shallow_copy_square = square.copy()
+        # Shallow copy of the group
+        shallow_copy_group = group.copy()
 
-        # Deep copy of the square
-        deep_copy_square = square.deepcopy()
+        # Deep copy of the group
+        deep_copy_group = group.deepcopy()
 
-        # Positioning the squares
-        shallow_copy_square.to_edge(LEFT)
-        deep_copy_square.to_edge(RIGHT)
+        # Positioning the groups
+        group.to_edge(UP)
+        shallow_copy_group.to_edge(LEFT)
+        deep_copy_group.to_edge(RIGHT)
 
         # Animate
-        self.play(ShowCreation(square))
-        self.play(ShowCreation(shallow_copy_square))
-        self.play(ShowCreation(deep_copy_square))
+        self.play(ShowCreation(group), ShowCreation(shallow_copy_group), ShowCreation(deep_copy_group))
 
-        # Change color of the original square
-        self.play(square.set_color, YELLOW)
+        # Change color of the circle in the original group
+        self.play(group[1].set_color, YELLOW)
 
-        # Notice how the shallow copy changes color, but the deep copy does not
+        # Notice how the shallow copy's circle changes color, but the deep copy's circle does not
         self.wait(2)
