8797633997cd882a6684fc38d090cea3d3d0ef9a
Ai Chain <hitthumit@gmail.com>
2023-11-29 01:21:17 +0800

    完整作品

---
+++ successful_tiktok/roate_test.py
@@ -0,0 +1,25 @@
+from manimlib import *
+
+class RotationDemo(Scene):
+    def construct(self):
+        # Create a square
+        square = Square()
+
+        # Add the square to the scene
+        self.add(square)
+
+        # Rotate the square 90 degrees (PI/2 radians) about its center
+        rotation_animation = Rotate(
+                                mobject=square, 
+                                angle=PI/2,
+                                axis=OUT,
+                                about_point=ORIGIN+RIGHT+UP
+                                )
+
+        dot = Dot(ORIGIN+RIGHT+UP)
+        self.add(dot)
+        # Play the animation
+        self.play(rotation_animation)
+
+        # Keep the scene displayed after animation
+        self.wait()
