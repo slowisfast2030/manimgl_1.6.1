2e2867c167cf26bf2297de09cf8a275915407207
Ai Chain <hitthumit@gmail.com>
2023-11-28 14:37:15 +0800

    完整作品

--- successful_tiktok/copy.py
+++ successful_tiktok/copy.py
@@ -5,6 +5,7 @@ class test(Scene):
         # Create a group of circles
         circle_group = VGroup(*[Circle(radius=0.5, color=WHITE) for _ in range(3)])
         circle_group.arrange(RIGHT, buff=1)
+        circle_group.test_list = [1,2,3]
 
         # Position the original group on the screen
         circle_group.to_edge(UP)
@@ -18,8 +19,12 @@ class test(Scene):
         deep_copy.to_edge(DOWN)
         
         circle_group[0].set_color(RED)
+        circle_group.test_list[0] = 'one'
 
         self.add(circle_group, shallow_copy, deep_copy)
+        print("circle_group.test_list", circle_group.test_list)
+        print("shallow_copy.test_list", shallow_copy.test_list)
+        print("deep_copy.test_list", deep_copy.test_list)
 
         # Display all the objects
         self.wait()
