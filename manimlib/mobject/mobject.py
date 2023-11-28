71ad7a340a384a6861f6a4dd3320920e0225a0db
Ai Chain <hitthumit@gmail.com>
2023-11-28 14:08:08 +0800

    完整作品

--- manimlib/mobject/mobject.py
+++ manimlib/mobject/mobject.py
@@ -745,6 +745,54 @@ class Mobject(object):
         return self
 
     # Copying
+    """
+    import copy
+
+    original = [[1, 2, 3], [4, 5, 6]]
+
+    # Shallow Copy
+    shallow = copy.copy(original)
+
+    # Deep Copy
+    deep = copy.deepcopy(original)
+
+    print("Original: ", original)
+    print("Shallow: ", shallow)
+    print("Deep: ", deep)
+
+    original[0][1] = 'two'
+
+    print("Original: ", original)
+    print("Shallow: ", shallow)
+    print("Deep: ", deep)
+    """
+    """
+    from manimlib import *
+
+    class test(Scene):
+        def construct(self):
+            # Create a group of circles
+            circle_group = VGroup(*[Circle(radius=0.5, color=WHITE) for _ in range(3)])
+            circle_group.arrange(RIGHT, buff=1)
+
+            # Position the original group on the screen
+            circle_group.to_edge(UP)
+
+            # Create a shallow copy and a deep copy of the group
+            shallow_copy = circle_group.copy()
+            deep_copy = circle_group.deepcopy()
+
+            # Position the copies
+            shallow_copy.center()
+            deep_copy.to_edge(DOWN)
+            
+            circle_group[0].set_color(RED)
+
+            self.add(circle_group, shallow_copy, deep_copy)
+
+            # Display all the objects
+            self.wait()
+    """
 
     def copy(self):
         # TODO, either justify reason for shallow copy, or
