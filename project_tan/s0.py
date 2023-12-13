fdd6879a10fd5e543de638c4c6ea9b3c2929dc26
Ai Chain <hitthumit@gmail.com>
2023-12-14 01:39:05 +0800

    完整作品

--- project_tan/s0.py
+++ project_tan/s0.py
@@ -199,7 +199,7 @@ class s0(Scene):
                     # Write(text_2),
                     # Write(text_3),
                   run_time=2)
-        self.wait()
+        #self.wait()
         
         rec_up = Rectangle(height=self.all_gr[0].get_height()+0.5, width=config.frame_width, color=BLACK, fill_opacity=0.6).move_to(self.all_gr[0])
         rec_mid = Rectangle(height=self.all_gr[1].get_height()+1.4, width=config.frame_width, color=BLACK, fill_opacity=0.6).move_to(self.all_gr[1])
@@ -210,11 +210,11 @@ class s0(Scene):
         # self.add(line)
 
         self.play(FadeIn(rec_mid), FadeIn(rec_down), run_time=1)
-        self.wait(2)
+        self.wait(1)
         self.play(FadeIn(rec_up), FadeOut(rec_mid), run_time=1)
-        self.wait(2)
+        self.wait(1)
         self.play(FadeIn(rec_mid), FadeOut(rec_down),run_time=1)
-        self.wait(2)
+        self.wait(1)
         self.play(FadeIn(rec_down))
         self.wait(2)
 
