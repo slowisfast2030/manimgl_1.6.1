22e698a1e3ed5850a0db720c32ce3bf90c3faca4
Ai Chain <hitthumit@gmail.com>
2023-12-15 19:23:01 +0800

    完整作品

--- project_tan/s3.py
+++ project_tan/s3.py
@@ -250,9 +250,11 @@ class s3(Scene):
 
         # 显示直线EF的方程和点D的坐标
         text3 = Tex(r"EF: y = -\frac{4}{3}x + \frac{4}{3}").scale(self.text_scale)
-        text3_res = Tex(r"D(0, \frac{4}{3})").scale(self.text_scale)
-        text3_gr = VGroup(text3, text3_res).arrange(RIGHT, buff=0.3).next_to(ORIGIN, DOWN, buff=0.8)
+        text3_res = Tex(r"\Rightarrow D(0, \frac{4}{3})").scale(self.text_scale)
+        text3_gr = VGroup(text3, text3_res).arrange(RIGHT, buff=0.3).next_to(self.plane, DOWN, buff=-3)
         self.play(Write(text3), run_time=1)
+        self.wait()
+        self.play(Write(text3_res), run_time=1)
         self.wait()
         # 显示最终结论
         text4 = Tex(r"tan(\frac{\alpha}{2}) = \frac{AD}{AC} = \frac{1}{3}").scale(self.text_scale).next_to(text3_gr, DOWN, buff=1)
