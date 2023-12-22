9a35b3d8a2cb99aeedb8e268c6513f58edbca743
Ai Chain <hitthumit@gmail.com>
2023-12-22 14:51:29 +0800

    完整作品

--- 3b1b-linus-creatures/alphabet_test.py
+++ 3b1b-linus-creatures/alphabet_test.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append("/Users/linus/Desktop/slow-is-fast/manimgl_1.6.1/3b1b-characters")
 
 from manimlib import *
-#from alphabet_creature import AlphabetCreature
 from alphabet_creature_upgrade import AlphabetCreature
 
 """
