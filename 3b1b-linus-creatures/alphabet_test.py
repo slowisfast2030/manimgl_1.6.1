4eaeadc6fb959d269c85335090a4e4c02c9b68ed
Ai Chain <hitthumit@gmail.com>
2023-12-22 15:02:32 +0800

    完整作品

--- 3b1b-linus-creatures/alphabet_test.py
+++ 3b1b-linus-creatures/alphabet_test.py
@@ -25,7 +25,7 @@ class test_body(Scene):
 """
 class test_omega(Scene):
     def construct(self):
-        a = AlphabetCreature(r"$a$", flip_at_start=False, 
+        a = AlphabetCreature(r"a", flip_at_start=False, 
                              color=RED,
                              eye_scale=0.3,
                              eye_buffer=0.09,
