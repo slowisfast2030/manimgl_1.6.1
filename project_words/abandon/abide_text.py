5f837bcb6b01d4d5adf75172e6fb430bfd734da8
Ai Chain <hitthumit@gmail.com>
2023-12-25 16:34:44 +0800

    完整作品

---
+++ project_words/abandon/abide_text.py
@@ -0,0 +1,14 @@
+'''
+abide
+1.(abide by)accept or act in accordance with (a rule, decision, or recommendation).
+2.If you <b>can't abide</b> someone or something, you dislike them very much. 
+
+
+I said I would abide by their decision.
+You'll have to abide by the rules of the club.
+Tom and Jerry abide by each other's agreement and have lived in peace ever since.
+
+
+I can't abide people who can't make up their minds.
+Ronaldo cannot abide Messi having more Ballon d'Ors than him, so Ronaldo continues to strive.
+'''
