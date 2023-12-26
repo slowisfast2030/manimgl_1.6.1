e5ac8de5d4c24a4f994cd6f1e5a690955cffe329
Ai Chain <hitthumit@gmail.com>
2023-12-27 00:36:59 +0800

    完整作品

---
+++ project_words/abide/abandon_text.py
@@ -0,0 +1,54 @@
+
+image_paths = ["abandon_boy.png",
+              "abandon_food2.png",
+              "abandon_path2.png"]
+
+student_words = ["What a lonely boy!",
+                 "Step down! Shame!",
+                 "Good choice!"]
+
+meaning_sentence_dict = {
+    "word": "abandon",
+    "attribute": "V-T",
+    "first_meaning": [
+        ["If you abandon a place, thing, or person, you", 
+         "leave the place, thing, or person permanently", 
+         "or for a long time, especially when you should", 
+         "not do so."],
+        ["如果你放弃一个地方、一件事或一个人，你",
+         "就永久地离开了这个地方、这件事或这个人，",
+         "尤其是当你不应该这样做的时候。"]
+    ],
+    "first_sentence": [
+        #["He claimed that his parents had abandoned", 
+        # "him."],
+        #["他声称他的父母已经抛弃了他。"],
+        ["Due to the divorce, the little boy's mother",
+        "abandoned him."],
+        ["由于离婚，这个小男孩的母亲抛弃了他。"]
+
+    ],
+    "second_meaning": [
+        ["If you abandon an activity or piece of work, you", 
+         "stop doing it before it is finished."],
+        ["如果你放弃一个活动或一项工作，意味着在",
+         "它完成之前你就停止了进行。尤其是当你不",
+         "应该这样做的时候。"]
+    ],
+    "second_sentence": [
+        ["The authorities have abandoned any attempt to", 
+         "distribute food in an orderly fashion."],
+        ["当局已放弃以有序方式分发食物的任何尝试。"] 
+    ],
+    "third_meaning": [
+        ["If you abandon an idea or way of thinking, you", 
+         "stop having that idea or thinking in that way."],
+        ["如果你放弃一个想法或思维方式，意味着你",
+         "停止拥有那个想法或以那种思维方式思考。"] 
+    ],
+    "third_sentence": [
+        ["Logic had prevailed and he had abandoned the", 
+         "idea of taking desolate path."],
+        ["逻辑占了上风，他放弃了走荒凉小路的想法。"]
+    ],
+}
