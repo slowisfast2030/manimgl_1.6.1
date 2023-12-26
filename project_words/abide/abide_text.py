19517d25a262ffc6af9555955fb6929ac81a205c
Ai Chain <hitthumit@gmail.com>
2023-12-27 00:54:26 +0800

    完整作品

--- project_words/abide/abide_text.py
+++ project_words/abide/abide_text.py
@@ -17,19 +17,23 @@ meaning_sentence_dict = {
     "word": "abide",
     "attribute": "V-T",
     "first_meaning": [
-        ["(abide by)accept or act in accordance with (a rule, decision, or recommendation)."],
+        ["(abide by)accept or act in accordance with (,",
+         "a rule decision, or recommendation)."],
         ["(遵守)接受或按照（规则、决定或建议）行事。"]
     ],
     "first_sentence": [
-        ["Tom and Jerry abide by each other's agreement and have lived in peace ever since."],
+        ["Tom and Jerry abide by each other's agreement",
+         "and have lived in peace ever since."],
         ["汤姆和杰瑞遵守彼此的协议，从此和平相处。"]
     ],
     "second_meaning": [
-        ["If you can't abide someone or something, you dislike them very much."],
+        ["If you can't abide someone or something, you",
+         "dislike them very much."],
         ["如果你不能忍受某人或某事，你非常讨厌他们。"]
     ],
     "second_sentence": [
-        ["Ronaldo cannot abide Messi having more Ballon d'Ors than him, so Ronaldo continues to strive."],
+        ["Ronaldo cannot abide Messi having more Ballon",
+         "d'Ors than him, so Ronaldo continues to strive."],
         ["罗纳尔多不能忍受梅西拥有比他更多的金球奖，",
          "所以罗纳尔多继续努力。"]
     ],
