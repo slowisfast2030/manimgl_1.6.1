04939d83c64ea3d2bbe589eb76786b5250e8594f
Ai Chain <hitthumit@gmail.com>
2023-12-26 23:51:00 +0800

    完整作品

--- project_words/abandon/word_tiktok_two.py
+++ project_words/abandon/word_tiktok_two.py
@@ -130,7 +130,6 @@ def student_with_teacher():
 def meaning(parts, parts_ch, sents, sents_ch):
     # parts是单词含义的各个部分，每个部分都是一个str
     VT = Text("V-T", font_size=40, t2c={'V-T': RED})
-    #VT = SVGMobject("svg-teapot.svg").scale(0.25)
     VT.move_to(VT_coord) 
 
     meaning_gr = [VT]
